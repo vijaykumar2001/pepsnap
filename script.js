8cc6d24bf208e289773fd580a797b5da75b02625
Vijaykumar Maradana <email@vijaykumarmaradana2001@gmail.com>
2022-06-07 14:50:35 +0530

    recording done

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let recordBtnCont = document.querySelector(".record-btn-cont")
 let captBtnCont = document.querySelector(".capture-btn-cont")
 let recordBtn = document.querySelector(".record-btn")
 let captBtn = document.querySelector(".capture-btn")
+let timer = document.querySelector(".timer")
 let recorder;
 let recordFlag = false;
 
 let constraint = {
-    video : false,  //true
-    audio : true   //true
+    video : true,  //true
+    audio : false   //true
 }
 
 navigator.mediaDevices.getUserMedia(constraint)
@@ -22,13 +23,19 @@ navigator.mediaDevices.getUserMedia(constraint)
   })
 
  recorder.addEventListener("dataavailable", (e) => {
-   chunks.push(e);
+   chunks.push(e.data);
  })
-
 
  
  recorder.addEventListener("stop", (e) => {
   //download the video 
+  let blob = new Blob(chunks, {type: "video/mp4"})
+  let videoURL = URL.createObjectURL(blob);
+  let a = document.createElement("a");
+  a.href = videoURL
+  a.download = "recordvideo.mp4"
+  a.click();
+  recordBtnCont.appendChild(a);
 })
 
 
@@ -44,13 +51,51 @@ recordBtnCont.addEventListener("click" , (e) => {
     {
       recorder.start();
       recordBtn.classList.add("anim-rec");
+      startTimer();
     }
 
     else
     {
       recorder.stop();
       recordBtn.classList.remove("anim-rec");
+      stopTimer();
     }
 
   
 })
+
+let counter =0;
+let timerID;
+function startTimer() {
+ timer.style.display = "block"
+  function displayTimer(){
+     let totalSeconds = counter;
+     let hours = Number.parseInt(totalSeconds / 3600);
+     totalSeconds = totalSeconds % 3600;
+
+     let minutes = Number.parseInt(totalSeconds / 60);
+     totalSeconds = totalSeconds % 60;
+
+     let seconds = totalSeconds;
+
+
+   hours = (hours < 10 ) ? `0${hours}` : hours;
+   minutes = (minutes < 10 ) ? `0${minutes}` : minutes;
+   seconds = (seconds < 10 ) ? `0${seconds}` : seconds;
+
+   timer.innerText = `${hours}:${minutes}:${seconds}`
+   counter++;
+   
+  }
+
+
+  timerID = setInterval(displayTimer, 1000)
+
+}
+
+function stopTimer() {
+  clearInterval(timerID);
+  timer.innerText = "00:00:00"
+  timer.style.display = "none";
+  
+}
