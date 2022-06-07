269596617bf55524d384c23cdf02f26a16fcbda0
Vijaykumar Maradana <email@vijaykumarmaradana2001@gmail.com>
2022-06-07 12:59:38 +0530

    recording part-1

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont")
+let captBtnCont = document.querySelector(".capture-btn-cont")
+let recordBtn = document.querySelector(".record-btn")
+let captBtn = document.querySelector(".capture-btn")
+let recorder;
+let recordFlag = false;
 
 let constraint = {
     video : false,  //true
-    audio : false   //true
+    audio : true   //true
 }
 
 navigator.mediaDevices.getUserMedia(constraint)
 .then((stream) => {
-  video.srcObject = stream
+  video.srcObject = stream;
+
+   recorder = new MediaRecorder(stream);
+
+  recorder.addEventListener("start", (e) => {
+    chunks = [];
+  })
+
+ recorder.addEventListener("dataavailable", (e) => {
+   chunks.push(e);
+ })
+
+
+ 
+ recorder.addEventListener("stop", (e) => {
+  //download the video 
+})
+
+
+})
+
+
+recordBtnCont.addEventListener("click" , (e) => {
+    if(!recorder) return;
+
+    recordFlag = !recordFlag;
+
+    if(recordFlag)
+    {
+      recorder.start();
+      recordBtn.classList.add("anim-rec");
+    }
+
+    else
+    {
+      recorder.stop();
+      recordBtn.classList.remove("anim-rec");
+    }
+
+  
 })
